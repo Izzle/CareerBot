998233fe71f056c1e55a71271f0494b08e59546e
Ron Martin <ronmartin89@gmail.com>
2017-10-26 01:46:42 -0700

    Added detailed header comment explaining the project.

---
+++ careerbot/__init__.py
@@ -0,0 +1,22 @@
+# CareerBot is a project that will get job postings off online
+# job boards (Angellist, Dice, Indeed, etc.) and sort the jobs.
+# Then it will create relevant statistics that the user can use
+# to set a 'baseline' for acceptable jobs that meant their criteria.
+# CareerBot will then weigh jobs based on a variety of factors.
+# Finally, CareerBot will send an email to each company in the top 
+# ranking percentiles with using a personalized template that inserts
+# relevant company information and attaches a resume.
+#
+# The goals of this project are to save countless hours that are 
+# normally spent job hunting, to become well-informed on the current
+# job market, and to deliver a quality personalized cover email
+
+# TODO: Perform A/B split testing of emails?
+# Create tests to make sure every template email is error free; REGEX ?
+# Create
+#
+#
+# Since I'm newer to Python coding, most of my code will initially
+# be in the __init__.py file. I know that this is bad practice, however,
+# I'm trying to focus on learning the program first. I'll go back and
+# fix my improper folder structuring afterwards.
