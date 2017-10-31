8b3a7ce5cedeae6a6ecd1d4590007a324ec6b8ed
Ron Martin <ronmartin89@gmail.com>
2017-10-30 18:45:04 -0700

    Fixed a bug where multiple next_siblings would throw an error.

--- careerbot/__init__.py
+++ careerbot/__init__.py
@@ -68,10 +68,13 @@ for div in soup2.find_all('div', class_=vgTop):
 print('*' * 20)
 
 print('Compensation:')
-for div in soup2.find_all('div', class_=vgBot):
-    if div.text == 'Compensation':
-        info = div.find_next_siblings('div', class_=vgBot)
-        print(info.contents)
+for main_div in soup2.find_all('div', class_=vgTop):
+    if main_div.text == 'Compensation':
+        # We iterate over the the elements because .string wont work on lists
+        # and a <br> would break the code
+        for info in main_div:
+            info = main_div.find_next_siblings('div', class_=vgBot)
+            print(info)
 
 
 # try:
